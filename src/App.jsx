87d2852c5f17403ea4563b9c65c794bf5948c1ca
jvariara <jvariara@gmail.com>
2023-09-20 20:59:35 -0400

    coming soon

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>hi</h1>
+      <div className="flex items-center flex-col">
+        <h1 className="text-5xl mb-4">Coming soon...</h1>
+        <h2 className="text-3xl">-Moth Medina</h2>
+      </div>
     </div>
   );
 }
